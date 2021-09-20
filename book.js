5d8156deb03ed15e8e1dc95e35e1bca652df7437
nislambsl <nazrulbsl7@gmail.com>
2021-09-20 21:04:26 +0600

    html done

--- book.js
+++ book.js
@@ -7,7 +7,35 @@ const searchBook = () => {
     
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data.docs))
+    .then(data => displaySeach(data.docs))
 
+}
+
+const displaySeach = docs => {
+   const output = document.getElementById('output');
+   docs.forEach(doc => {
+       
+       const div = document.createElement('div');
+       div.classList.add('col');
+       const imgUrl = ` https://covers.openlibrary.org/b/id/${doc.cover_i}-S.jpg`
+  
+       div.innerHTML = `  
+       <div class="card">
+       <img class="img-fluid img-thumbnail" src="${imgUrl}" class="card-img-top" alt="...">
+       <div class="card-body">
+         <h5 class="card-title">${doc.title}</h5>
+
+       <p>${doc.author_name}</p>
+       <p>${doc.publisher}</p>
+       <p>${doc.first_publish_year}</p>
+      
+     
+       </div>
+     </div>
+     `;
+     output.appendChild(div);
+
+
+   });
 }
 
